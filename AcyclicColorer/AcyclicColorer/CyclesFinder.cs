3aeec27b66294de8561a8f5f43466bf0012c9841
Ahmed Abdelkarim <a.abdelkarim@student.mini.pw.edu.pl>
2017-06-27 18:18:19 +0200

    cykle

---
+++ AcyclicColorer/AcyclicColorer/CyclesFinder.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcyclicColorer
+{
+	public class CyclesFinder
+	{
+		private Graph Graph;
+		private List<List<Vertex>> Cycles = new List<List<Vertex>>();
+
+		public CyclesFinder(Graph graph)
+		{
+			Graph = graph;
+		}
+
+		public List<List<Vertex>> FindCycles() //metoda oparta o DFS
+		{
+			foreach (var v in Graph.Vertices)
+				FindNewCycles(new List<Vertex>{v});
+
+			return Cycles;
+		}
+
+		private void FindNewCycles(List<Vertex> path)
+		{
+			var n = path[path.Count - 1];
+
+			foreach (var x in n.Edges)
+			{
+				if (!path.Contains(x)) //dodajemy x do ścieżki
+				{
+					var p = new List<Vertex>(path);
+					p.Add(x);
+					FindNewCycles(p);
+				}
+				else if ((path.Count > 2) && (x == path[0])) //znaleźliśmy cykl
+				{
+					var p = Normalize(path);
+					var invP = Invert(p);
+					if (IsNew(p) && IsNew(invP))
+						Cycles.Add(p);
+				}
+			}
+		}
+
+		private bool AreEqual(List<Vertex> l, List<Vertex> r)
+		{
+			if (l.Count != r.Count)
+				return false;
+
+			bool ret = true;
+			for (int i = 0; i < l.Count; ++i)	//równość ze zgodną kolejnością
+				if (l[i].Index != r[i].Index)
+				{
+					return false;
+				}
+			return true;
+		}
+
+		private List<Vertex> Invert(List<Vertex> path)
+		{
+			var p = new List<Vertex>(path);
+			p.Reverse();
+			return Normalize(p);
+		}
+
+		private List<Vertex> Normalize(List<Vertex> path) //obrócenie tak, żeby cykl zaczynał się od najmniejszego wierzchołka
+		{
+			int minPos = 0;
+			for (int i = 0; i < path.Count; ++i)
+			{
+				if (path[i].Index < path[minPos].Index)
+					minPos = i;
+			}
+
+			return path.Skip(minPos).Concat(path.Take(minPos)).ToList();
+		}
+
+		private bool IsNew(List<Vertex> path)
+		{
+			foreach (var c in Cycles)
+				if (AreEqual(c, path))
+					return false;
+
+			return true;
+		}
+	}
+}
---
+++ AcyclicColorer/AcyclicColorerTests/GraphTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using AcyclicColorer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AcyclicColorerTests
+{
+	[TestClass]
+	public class GraphTests
+	{
+		private string Path = @"..\..\..\TestGraphs\";
+
+		[TestMethod]
+		public void LoadingFromFileTest()
+		{
+			var expectedEdges = new List<Tuple<int, int>>
+			{
+				new Tuple<int, int>(0, 1),
+				new Tuple<int, int>(0, 2),
+				new Tuple<int, int>(0, 3),
+				new Tuple<int, int>(1, 2),
+				new Tuple<int, int>(2, 3),
+				new Tuple<int, int>(1, 4),
+				new Tuple<int, int>(3, 4),
+				new Tuple<int, int>(5, 6),
+				new Tuple<int, int>(6, 7),
+				new Tuple<int, int>(5, 7)
+			};
+
+			var g = Graph.FromFile(Path + "StackTestGraph.txt");
+			Assert.AreEqual(8, g.Vertices.Count);
+			var edgeIndices = (from e in g.GetAllEdges() select new Tuple<int, int>(e.Item1.Index, e.Item2.Index)).ToList();
+			Assert.AreEqual(expectedEdges.Count, edgeIndices.Count);
+			foreach (var e in expectedEdges)
+			{
+				Assert.IsTrue(edgeIndices.Any(v => (v.Item1 == e.Item1 && v.Item2 == e.Item2) || (v.Item1 == e.Item2 && v.Item2 == e.Item1)));
+			}
+		}
+	}
+}
